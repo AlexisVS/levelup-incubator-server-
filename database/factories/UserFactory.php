2f28e9fc3f9c7c52a24f4b2353853bfa608e436d
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-02-18 12:15:16 +0100

    update faker user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -16,7 +17,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'email' => 'admin@admin.com',
+            'email' => $this->faker->email(),
             'email_verified_at' => now(),
             'password' => bcrypt('admin@admin.com'), // password
             'remember_token' => Str::random(10),
